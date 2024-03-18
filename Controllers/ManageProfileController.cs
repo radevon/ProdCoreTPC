d7b104e8f7da6ee8fbcd979c395024f879b5e4b9
radevon <radevon@mail.ru>
2024-03-18 16:24:41 +0300

    add remove, edit button to users

--- Controllers/ManageProfileController.cs
+++ Controllers/ManageProfileController.cs
@@ -13,7 +13,7 @@ using ProdCoreTPC.Code.Repositories;
 using ProdCoreTPC.Models;
 using ProdCoreTPC.Code;
 using System.Data;
-
+using Microsoft.Extensions.Configuration;
 
 namespace ProdCoreTPC.Controllers
 {
@@ -21,20 +21,19 @@ namespace ProdCoreTPC.Controllers
     public class ManageProfileController : Controller
     {
 
-        private IRepository<ApplicationUser, String> userRepo;
-        //private IRepository<IdentityRole, String> roleRepo;
+        
         private RoleManager<IdentityRole> roleManager;
 
         private UserManager<ApplicationUser> userManager;
 
+        private IConfiguration configuration;
 
 
-        public ManageProfileController(IRepository<ApplicationUser, String> _userRepo, IRepository<IdentityRole, String> _roleRepo, RoleManager<IdentityRole> _roleManager, UserManager<ApplicationUser> userManager)
+        public ManageProfileController(RoleManager<IdentityRole> _roleManager, UserManager<ApplicationUser> userManager, IConfiguration configuration)
         {
-            this.userRepo = _userRepo;
-            //this.roleRepo = _roleRepo;
             this.roleManager = _roleManager;
             this.userManager = userManager;
+            this.configuration = configuration;
         }
 
         public IActionResult UserList()
@@ -175,6 +174,8 @@ namespace ProdCoreTPC.Controllers
                 return PartialView("_AddProfile", newProfile);
             }
             ApplicationUser user = await userManager.FindByNameAsync(newProfile.UserName);
+            
+            
             if (user != null)
             {
                 ModelState.AddModelError("","Пользователь с таким именем (логином) уже существует. Придумайте другой логин!");
@@ -196,7 +197,66 @@ namespace ProdCoreTPC.Controllers
             }
 
         }
+        [HttpGet]
+        public async Task<IActionResult> DeleteProfile(string userId)
+        {
+            ApplicationUser userToDeleted = await userManager.FindByIdAsync(userId);
+            if (userToDeleted == null)
+            {
+                return Content($"Пользователь не найден");
+            }
 
-        
+            return PartialView("_DeleteProfile", userToDeleted);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteProfileConfirm(string userId)
+        {
+            ApplicationUser userToDeleted = await userManager.FindByIdAsync(userId);
+            if (userToDeleted == null)
+            {
+                return Content($"Пользователь не найден");
+            }
+
+            if (User.Identity.Name == userToDeleted.UserName)
+            {
+                return Content($"Собственный аккаунт невозможно удалить");
+            }
+
+            string adminUser = configuration.GetValue<string>("AdministratorAccount:Login");
+            if (userToDeleted.UserName==adminUser)
+            {
+                return Content($"Аккаунт администратора запрещено удалять");
+            }
+
+            IdentityResult result = await userManager.DeleteAsync(userToDeleted);
+
+            if (result.Succeeded)
+            {
+                return Content($"Пользователь '{userToDeleted.UserName}' успешно удалён! Страница будет перезагружена" + ScriptConstants.RELOAD_SCRIPT);
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return PartialView("_DeleteProfile", userToDeleted);
+            }
+        }
+
+        public async Task<IActionResult> ChangeProfileRoles(string userId)
+        {
+            ApplicationUser user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return Content($"Пользователь не найден");
+            }
+
+            List<string> allRoles = roleManager.Roles.Select(x => x.Name).OrderBy(x => x).ToList();
+
+            return Content("Роли");
+        }
+
     }
 }
---
+++ Models/ManageProfile/UserRolesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdCoreTPC.Models
+{
+    // модель для редактирования ролей пользователя
+    public class UserRolesModel
+    {
+        public string UserId { get; set; }
+
+        public Dictionary<string,bool> Roles { get; set; }
+    }
+}
